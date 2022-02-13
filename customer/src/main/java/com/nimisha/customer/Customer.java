46468eeb67ae19f32ba530b909515856213e80be
nimisha1796 <46665062+nimisha1796@users.noreply.github.com>
2022-02-13 22:26:03 +0530

    Initial commit---Customer

---
+++ customer/src/main/java/com/nimisha/customer/Customer.java
@@ -0,0 +1,25 @@
+package com.nimisha.customer;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence",
+    sequenceName = "customer_id_sequence")
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "customer_id_sequence")
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ customer/src/main/java/com/nimisha/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,9 @@
+package com.nimisha.customer;
+
+public record CustomerRegistrationRequest(
+
+        String firstName,
+        String lastName,
+        String email
+) {
+}
